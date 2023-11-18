8e92204251c20586c0f5b862214007ec72a8067d
mgm-ok <151377452+mgm-ok@users.noreply.github.com>
2023-11-18 16:13:11 +0300

    Add files via upload

---
+++ style.js
@@ -0,0 +1,13 @@
+function toggleText() {
+    var x = document.getElementById("button1");
+    const element = document.querySelector('body')
+        element.classList.add('back');
+        element.classList.remove('bakc');
+  }
+
+function toggleText1() {
+    var y = document.getElementById("button2");
+    const elem = document.querySelector('body')
+      elem.classList.remove('back');
+      elem.classList.add('bakc');
+  }
